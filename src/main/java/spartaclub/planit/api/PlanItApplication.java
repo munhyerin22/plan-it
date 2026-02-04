7bbf0564fc052779837245979b93993c5bfde2be
이름 <>
2026-02-05 00:14:49 +0900

    초기 커밋

---
+++ src/main/java/spartaclub/planit/api/PlanItApplication.java
@@ -0,0 +1,13 @@
+package spartaclub.planit.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanItApplication.class, args);
+    }
+
+}
